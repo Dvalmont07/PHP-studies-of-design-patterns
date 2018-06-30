b2012ab5ffb9dc38ecab066c9ea11c0ad3f4739d
Danilo Matos <danilo.dejesusmatos@gmail.com>
2018-06-30 03:16:37 -0300

    Adiidng Store class

---
+++ observer/Store.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * PHP version 7.1
+ * Ibserver - Interface that serves the observers type classes
+ *
+ * @category  Abstract
+ * @package   Observer
+ * @author    Danilo Lobo Matos <danilo.dejesusmatos@gmail.com>
+ * @copyright 2018 Danilo Lobo Matos / Dvalmont07
+ * @license   http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version   GIT: <git_id>
+ * @link      "http://www.seoseedrank.com.br/sobre"
+ * @since     1.0.0
+ */
+
+/**
+ * PHP version 7.1
+ * Ibserver - Interface that serves the observers type classes
+ *
+ * @category  Abstract
+ * @package   Observer
+ * @author    Danilo Lobo Matos <danilo.dejesusmatos@gmail.com>
+ * @copyright 2018 Danilo Lobo Matos / Dvalmont07
+ * @license   http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version   Release: <git_id>
+ * @link      "http://www.seoseedrank.com.br/sobre"
+ * @since     1.0.0
+ */
+abstract class Store
+{
+    public $className;
+}
