8a5f2db972230c582faeb3c6677fd06db81c4543
Danilo Lobo Matos <40126858+Dvalmont07@users.noreply.github.com>
2018-08-20 03:08:25 -0300

    Changing IEnemy into abstract class

--- factory/Enemy.php
+++ factory/Enemy.php
@@ -1,39 +1,37 @@
 <?php
 /**
- * PHP version 7.1
- * Ibserver - Interface that serves the observers type classes
- *
- * @category  Inteface
- * @package   Observer
+ * PHP version 7.2.4
+ * Enemy - Interface class
+ * 
+ * @category  Abstract
+ * @package   Factory
  * @author    Danilo Lobo Matos <danilo.dejesusmatos@gmail.com>
  * @copyright 2018 Danilo Lobo Matos / Dvalmont07
  * @license   http://opensource.org/licenses/gpl-license.php GNU Public License
- * @version   GIT: <git_id>
  * @link      "http://www.seoseedrank.com.br/sobre"
  * @since     1.0.0
  */
 
 /**
- * PHP version 7.1
- * Ibserver - Interface that serves the observers type classes
- *
- * @category  Inteface
- * @package   Observer
+ * Enemy - Interface class
+ * 
+ * @category  Abstract
+ * @package   Factory
  * @author    Danilo Lobo Matos <danilo.dejesusmatos@gmail.com>
  * @copyright 2018 Danilo Lobo Matos / Dvalmont07
  * @license   http://opensource.org/licenses/gpl-license.php GNU Public License
- * @version   Release: <git_id>
  * @link      "http://www.seoseedrank.com.br/sobre"
  * @since     1.0.0
  */
-interface IObserver
-{
-    /**
-     * Use updated data
-     *
-     * @param array $products list of products
-     * 
-     * @return void
-     */
-    public function update($products);
-}
+
+ abstract class IEnemy{
+    protected $name;
+    protected $damege;
+
+    protected function setName(String $n){
+        $name = $n;
+    }
+
+
+    
+ }
