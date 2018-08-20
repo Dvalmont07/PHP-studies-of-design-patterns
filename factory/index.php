d2cb99ac10e9af51dd155bd2a79bde4e85cc9bed
Danilo Lobo Matos <40126858+Dvalmont07@users.noreply.github.com>
2018-08-20 03:00:07 -0300

    Adding Factory Classes

---
+++ factory/index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * PHP version 7.2.4
+ * Index - Root file for Factory Pattern
+ * 
+ * @category  File
+ * @package   Factory
+ * @author    Danilo Lobo Matos <danilo.dejesusmatos@gmail.com>
+ * @copyright 2018 Danilo Lobo Matos / Dvalmont07
+ * @license   http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version   GIT: <git_id>
+ * @link      "http://www.seoseedrank.com.br/sobre"
+ * @since     1.0.0
+ */
+
+require_once '../config.php';
+require_once '../autoload.php';
+
+
+
+
